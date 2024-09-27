3f734d1a4bb7a7d0bd0f626c49fb1cd47bbfcd83
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-09-27 16:11:29 -0600

    Add files via upload

---
+++ eva1_13_aguila_sol/EVA1_13_AGUILA_SOL.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_13_aguila_sol;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA1_13_AGUILA_SOL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //ALEATORIO
+        double valor = Math.random();
+        //random genera valores valores > = 0 y < a 1
+        //entre o y .9999999999999999
+        if(valor < 0.5){
+            System.out.println("aguila");
+                    
+        }else{
+            System.out.println("sol");
+        }   
+            
+            
+            
+        
+            
+        
+    }
+    
+}
